6e96993503f4fb3096290d2a9b0f90e95cbf78b1
Simon Lund <simon97lund@gmail.com>
2024-12-27 17:03:28 +0100

    :recycle: move kbd styling to global css file

--- src/lib/components/copy-toast.svelte
+++ src/lib/components/copy-toast.svelte
@@ -2,24 +2,14 @@
 	import { CopyIcon } from 'lucide-svelte';
 
 	export let toast: { appName: string };
-
 </script>
 
-<style>
-		.toast {
-				@apply flex flex-col gap-2;
-		}
-    kbd {
-        @apply bg-muted text-muted-foreground pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100;
-    }
-</style>
-
 <div class="toast">
-	<div class="flex gap-2 items-center">
-		<CopyIcon class="min-h-4 h-4 min-w-4 w-4 inline-block text-green-500" strokeWidth="3" />
+	<div class="flex items-center gap-2">
+		<CopyIcon class="inline-block h-4 min-h-4 w-4 min-w-4 text-green-500" strokeWidth="3" />
 		<span><strong>{toast.appName}</strong> copied to clipboard.</span>
 	</div>
-	<ul class="text-muted-foreground text-sm list-decimal list-inside">
+	<ul class="list-inside list-decimal text-sm text-muted-foreground">
 		<li>
 			Open your file explorer (<kbd>&#x229E; Win</kbd> + <kbd>E</kbd>).
 		</li>
@@ -31,3 +21,9 @@
 		</li>
 	</ul>
 </div>
+
+<style>
+	.toast {
+		@apply flex flex-col gap-2;
+	}
+</style>
