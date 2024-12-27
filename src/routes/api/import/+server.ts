fa89a2768b97fb225282e5f40c1584404ff9bc99
Simon Lund <simon97lund@gmail.com>
2024-12-27 19:54:22 +0100

    :sparkles: fix import endpoint

--- src/routes/api/import/+server.ts
+++ src/routes/api/import/+server.ts
@@ -1,32 +1,24 @@
 import { HTTP_STATUS } from '$lib/constants';
 import { error, json } from '@sveltejs/kit';
 import { STEAM_API_KEY } from '$env/static/private';
-import appsFile from '$lib/assets/apps.json';
-import type { TApp, TAppsFile } from '$lib/types';
+import type { TApp } from '$lib/types';
 
-// Map of app IDs to app names
-// appsFile has type { appid: number, name: string }[]
-const appsMap = (appsFile as TAppsFile).reduce((acc, app) => {
-	acc[app.appid] = { id: app.appid, name: app.name };
-	return acc;
-}, {} as Record<number, TApp>);
+type TOwnedGamesAPIResponse = {
+	response: { games: { appid: number; name: string; playtime_forever: number }[] };
+};
 
 export async function GET({ url }) {
 	const steamId = url.searchParams.get('steamId');
 	if (!steamId) error(HTTP_STATUS.BAD_REQUEST, 'No query parameter (\'steamId\') provided');
 
 	// Fetch games from Steam API
-	const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamId}&format=json`);
-	const data: { response: { games: { appid: number, playtime_forever:number }[] } } = await response.json();
+	const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamId}&format=json&include_appinfo=1`);
+	const data: TOwnedGamesAPIResponse = await response.json();
 
 	// Sort games by "playtime_forever" and get app IDs
-	const appIds: number[] = data.response.games
+	const apps: TApp[] = data.response.games
 		.sort((a, b) => b.playtime_forever - a.playtime_forever)
-		.map(app => app.appid);
-
-	// Get names of games
-	// TODO: why are not all games found????
-	const apps = appIds.map(appId => appsMap[appId]).filter(Boolean);
+		.map(app => ({ id: app.appid, name: app.name }));
 
 	return json(apps);
 }
