cc4f0dba3c045adc3f1e6626e393d162655a7d93
Simon Lund <simon97lund@gmail.com>
2024-12-27 18:23:39 +0100

    :recycle: rework settings dialog
    - add "import apps" button
    - rework structure

--- src/lib/components/settings-dialog.svelte
+++ src/lib/components/settings-dialog.svelte
@@ -7,6 +7,7 @@
 
 	import { appsStore, settingsStore } from '../stores';
 	import { dialogState } from '$lib/state';
+	import { importApps } from '$lib/stores/apps-store.js';
 
 	function close() {
 		dialogState.showSettingsDialog = false;
@@ -26,24 +27,40 @@
 			<Dialog.Title>Settings</Dialog.Title>
 			<Dialog.Description>Manage your settings here.</Dialog.Description>
 		</Dialog.Header>
-		<div class="space-y-4">
+		<div class="space-y-8">
 			<div>
+				<h2 class="text-lg font-medium">Steam ID</h2>
+				<p class="text-muted-foreground text-sm mb-2">
+					The Steam ID is used to generate the correct path to the screenshots folder of your Steam apps.
+					Your Steam ID is securely stored in your browser and is not shared with anyone.
+					<a
+						href="/help/add-steamid"
+						class="text-sm text-blue-500 hover:underline"
+						onclick={() => close()}
+					>
+						Don't know your Steam ID?
+					</a>
+				</p>
 				<Label for="name">Steam Id</Label>
 				<Input id="name" bind:value={$settingsStore.steamId} class="col-span-3" required placeholder="Your Steam ID" />
-				<a
-					href="/help/add-steamid"
-					class="text-sm text-blue-500 hover:underline"
-					onclick={() => close()}
-				>
-					Don't know your Steam ID?
-				</a>
 			</div>
-			<div class="space-y-2 rounded border border-destructive p-4">
-				<p>
-					<span class="font-medium text-destructive">Warning:</span> This will remove all your apps.
-					This action cannot be undone.
-				</p>
-				<Button on:click={reset} variant="destructive">Remove All Apps</Button>
+			<div>
+				<h2 class="text-lg font-medium">App Management</h2>
+				<div class="space-y-2 mb-6">
+					<p class="text-muted-foreground text-sm">
+						Here you can import all your apps from Steam. This will merge your current apps with the imported ones.
+					</p>
+					<Button on:click={() => importApps($settingsStore.steamId)} class="w-full">Import Apps</Button>
+				</div>
+				<div class="space-y-2 rounded border border-destructive p-4">
+					<p>
+						<span class="font-medium text-destructive">Warning:</span> This will remove all your apps.
+						This action cannot be undone.
+					</p>
+					<div class="flex justify-end">
+						<Button on:click={reset} variant="destructive">Remove All Apps</Button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</Dialog.Content>
